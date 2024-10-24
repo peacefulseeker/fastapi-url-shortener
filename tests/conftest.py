73bc2b970bfb6258e84fd78299e31ec92d4aa42b
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-10-24 12:00:52 +0300

    Tests with Mocked DynamoDB resource (#4)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
+from typing import TYPE_CHECKING, Generator
+
+import boto3
 import pytest
 from fastapi.testclient import TestClient
+from moto import mock_aws
+
+if TYPE_CHECKING:
+    from mypy_boto3_dynamodb import DynamoDBServiceResource
 
 from tests.db import TestDDB
 
+from app.config import settings
 from app.main import app
 
 
@@ -11,9 +19,21 @@ def client() -> TestClient:
     return TestClient(app)
 
 
+@pytest.fixture
+def ddb_resource() -> Generator["DynamoDBServiceResource", None, None]:
+    with mock_aws():
+        conn = boto3.resource("dynamodb", region_name=settings.aws_region_name)
+        yield conn
+
+
+@pytest.fixture(autouse=True)
+def get_ddb_resource(mocker, ddb_resource):
+    return mocker.patch("app.db.get_ddb_resource", return_value=ddb_resource)
+
+
 @pytest.fixture()
-def ddb():
-    ddb = TestDDB()
+def ddb(ddb_resource):
+    ddb = TestDDB(ddb_resource)
     ddb.create()
     yield ddb
     ddb.delete()
--- tests/db.py
+++ tests/db.py
@@ -1,15 +1,14 @@
 from typing import TYPE_CHECKING
 
 from app.config import settings
-from app.db import get_ddb_resource
 
 if TYPE_CHECKING:
     from mypy_boto3_dynamodb.service_resource import DynamoDBServiceResource, Table
 
 
 class TestDDB:
-    def __init__(self):
-        self.ddb: "DynamoDBServiceResource" = get_ddb_resource()
+    def __init__(self, ddb_resource: "DynamoDBServiceResource"):
+        self.ddb = ddb_resource
         self.table_name = settings.ddb_table_name
         self.table: None | Table = None
 
