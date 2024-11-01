34fec66294a100daa528742fa9e24f97f14471e1
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-11-01 11:24:37 +0200

    put onBeforeSwap logic in form component itself

--- frontend/src/components/form.ts
+++ frontend/src/components/form.ts
@@ -1,3 +1,7 @@
+import { Toast, ToastLevel } from "../components/toast";
+import { ShortenedUrlResponse } from "../types";
+import copy from "copy-to-clipboard";
+
 export class Form extends HTMLElement {
   private submitBtn: HTMLButtonElement;
 
@@ -13,6 +17,41 @@ export class Form extends HTMLElement {
       this.submitBtn.classList.remove("btn-form-submitting");
     }, 200);
   };
+
+  onBeforeSwap = (evt: CustomEvent) => {
+    const status = evt.detail.xhr.status;
+    const toast = document.querySelector("c-toast") as Toast;
+
+    let responseMessage;
+    try {
+      responseMessage = JSON.parse(evt.detail.serverResponse)["detail"];
+    } catch {
+      responseMessage = evt.detail.serverResponse;
+    }
+
+    if (responseMessage.shortened_url) {
+      this.processUrlShortened(responseMessage, toast);
+    } else if (status >= 400 && status < 500) {
+      toast.show(responseMessage, ToastLevel.WARNING);
+    } else if (status >= 500) {
+      toast.show("Something went wrong, please try again later.", ToastLevel.ERROR);
+    }
+  };
+
+  processUrlShortened(responseMessage: ShortenedUrlResponse, toast: Toast) {
+    const copied = copy(responseMessage.shortened_url);
+    let toastMessage = `Short URL <pre class="break-all text-sm font-bold whitespace-pre-wrap">${responseMessage.shortened_url}</pre> created`;
+    if (copied) {
+      toastMessage += `and copied to clipboard!`;
+      if (responseMessage.expires_at) {
+        const expiresAt = new Date(responseMessage.expires_at * 1000).toLocaleString();
+        toastMessage += `<br />With expiration date set to ${expiresAt}`;
+      }
+    } else {
+      toastMessage = `Short URL <pre class="break-all text-sm font-bold whitespace-pre-wrap">${responseMessage.shortened_url}</pre> created succesfully!`;
+    }
+    toast.show(toastMessage, ToastLevel.SUCCESS, null);
+  }
 }
 
 customElements.define("c-form", Form);
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,5 @@
 import "htmx.org"; // prettier-ignore
 
 import "./style.css";
-import "./handlers/htmx.beforeSwap";
 import "./components/toast";
 import "./components/form";
