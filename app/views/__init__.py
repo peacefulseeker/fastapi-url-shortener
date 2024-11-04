4ba7f514c2ba6cf3d1b6e80b7c390cabab4a8a26
Alexey Vorobyov <alexey.vorobyov.s@gmail.com>
2024-11-04 16:09:42 +0200

    increment url visits in a background task

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,10 +1,11 @@
 import starlette.status as status
-from fastapi import Request
+from fastapi import BackgroundTasks, Request
 from fastapi.responses import RedirectResponse
 from starlette.templating import _TemplateResponse
 
 from app.config import settings, templates
 from app.db import GetDBTable
+from app.tasks import increment_url_visits
 
 
 def get_frontend_assets_url() -> str:
@@ -29,17 +30,13 @@ def home(request: Request) -> _TemplateResponse:
     )
 
 
-def catch_all_redirect(path: str, request: Request, table: GetDBTable) -> RedirectResponse:
+def catch_all_redirect(path: str, request: Request, table: GetDBTable, background_tasks: BackgroundTasks) -> RedirectResponse:
     response = table.get_item(Key={"ShortPath": path})
 
     if "Item" not in response:
         return RedirectResponse(request.url_for("home"), status_code=status.HTTP_304_NOT_MODIFIED)
 
-    table.update_item(
-        Key={"ShortPath": path},
-        UpdateExpression="ADD Visits :inc",
-        ExpressionAttributeValues={":inc": 1},
-    )
-
     full_url = str(response["Item"]["FullUrl"])
+    background_tasks.add_task(increment_url_visits, path, table)
+
     return RedirectResponse(full_url, status_code=status.HTTP_302_FOUND)
